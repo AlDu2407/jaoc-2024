45bcb9f70e724fa63c04dbd0b65e3a9c2ffd192a
Al Du <drachendolch@gmail.com>
2024-12-19 18:37:37 +0100

    Implemented solution for the second task of day 09.

--- src/main/java/org/aldu/jaoc/Main.java
+++ src/main/java/org/aldu/jaoc/Main.java
@@ -1,13 +1,13 @@
 import org.aldu.jaoc.solutions.*;
 
 public static void main(String[] _args) {
-  //  new DayOne().executeTasks();
-  //  new DayTwo().executeTasks();
-  //  new DayThree().executeTasks();
-  //  new DayFour().executeTasks();
-  //  new DayFive().executeTasks();
-  //  new DaySix().executeTasks();
-  //  new DaySeven().executeTasks();
-  //  new DayEight().executeTasks();
-  new DayNine().executeTasks();
+//    new DayOne().executeTasks();
+//    new DayTwo().executeTasks();
+//    new DayThree().executeTasks();
+//    new DayFour().executeTasks();
+//    new DayFive().executeTasks();
+//    new DaySix().executeTasks();
+//    new DaySeven().executeTasks();
+//    new DayEight().executeTasks();
+    new DayNine().executeTasks();
 }
--- src/main/java/org/aldu/jaoc/solutions/DayNine.java
+++ src/main/java/org/aldu/jaoc/solutions/DayNine.java
@@ -1,12 +1,14 @@
 package org.aldu.jaoc.solutions;
 
+import java.util.*;
+import java.util.stream.IntStream;
 import org.aldu.jaoc.utils.FileUtils;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-
 public class DayNine extends AbstractDay {
+  private record Space(int startIndex, int size) {}
+
+  private record FileData(int fileId, Space space) {}
+
   @Override
   protected String getDay() {
     return "day09";
@@ -15,13 +17,7 @@ public class DayNine extends AbstractDay {
   @Override
   protected void taskOne() {
     var input = FileUtils.readInputAsLine(getInputFileName());
-    var sum =
-        IntStream.range(0, input.length())
-            .map(input::charAt)
-            .mapToObj(Character::toString)
-            .map(Integer::parseInt)
-            .reduce(0, Integer::sum);
-
+    var sum = calculateSum(input);
     var memory = new Integer[sum];
     var fileId = 0;
     var idx = 0;
@@ -46,17 +42,82 @@ public class DayNine extends AbstractDay {
       i++;
       j--;
     }
-    var result = 0L;
-    var n = 0;
-    while (memory[n] != -1) {
-      result += n * (long) memory[n];
-      n++;
-    }
+
+    var result = calculateResult(memory);
     printResult(Task.ONE, result);
   }
 
   @Override
   protected void taskTwo() {
-    printResult(Task.TWO, 0L);
+    var input = FileUtils.readInputAsLine(getInputFileName());
+    var sum = calculateSum(input);
+    var memory = new Integer[sum];
+    var freeSpaces = new ArrayList<Space>();
+    var files = new ArrayList<FileData>();
+    var fileId = 0;
+    var idx = 0;
+    for (var i = 0; i < input.length(); i++) {
+      var isFileBlock = i % 2 == 0;
+      var size = Integer.parseInt(Character.toString(input.charAt(i)));
+      if (isFileBlock) {
+        files.add(new FileData(fileId, new Space(idx, size)));
+      } else {
+        freeSpaces.add(new Space(idx, size));
+      }
+      fileId = isFileBlock ? fileId + 1 : fileId;
+      idx += size;
+    }
+
+    for (var i = files.size() - 1; i >= 0; i--) {
+      var file = files.get(i);
+      for (var j = 0; j < freeSpaces.size(); j++) {
+        var freeSpace = freeSpaces.get(j);
+        if (freeSpace.startIndex() >= file.space().startIndex()) {
+          break;
+        }
+        if (freeSpace.size() >= file.space().size()) {
+          var _ = files.remove(i);
+          files.add(
+              i,
+              new FileData(file.fileId(), new Space(freeSpace.startIndex(), file.space().size())));
+          int spaceDifference = freeSpace.size() - file.space().size();
+          if (spaceDifference > 0) {
+            var _ = freeSpaces.remove(j);
+            freeSpaces.add(
+                j, new Space(freeSpace.startIndex() + file.space().size(), spaceDifference));
+          } else {
+            var _ = freeSpaces.remove(j);
+          }
+          break;
+        }
+      }
+    }
+
+    for (var file : files) {
+      for (var i = 0; i < file.space().size; i++) {
+        memory[file.space().startIndex() + i] = file.fileId();
+      }
+    }
+
+    var result = calculateResult(memory);
+    printResult(Task.TWO, result);
+  }
+
+  private int calculateSum(String input) {
+    return IntStream.range(0, input.length())
+        .map(input::charAt)
+        .mapToObj(Character::toString)
+        .map(Integer::parseInt)
+        .reduce(0, Integer::sum);
+  }
+
+  private long calculateResult(Integer[] memory) {
+    var result = 0L;
+    for (var n = 0; n < memory.length; n++) {
+      if (memory[n] != null && memory[n] != -1) {
+        result += n * (long) memory[n];
+      }
+    }
+    return result;
   }
 }
